4d7bca3e049ddbdf905b24252ff3e375b26e3499
Pedro Xavier <pedroxxavier@hotmail.com>
2023-08-22 14:30:56 -0300

    Constructor e Renderização

--- src/index.js
+++ src/index.js
@@ -1,42 +1,10 @@
 import { Personagem } from "./modules/personagem.js";
+import { PersonagemView } from "./components/personagem-view.js";
+import { Mago } from "./modules/mago.js";
 
-// const personagemPedrinho = {
-//   nome: "Pedrinho",
-//   vida: 7,
-//   mana: 12,
-//   level: 5,
-//   tipo: "Mago",
-// };
+const personagemPedrinho = new Personagem("Pedrinho", 5, "Mago");
+const personagemJose = new Personagem("José", 3, "Arqueiro");
 
-// const personagemJose = {
-//   nome: "Jose",
-//   vida: 7,
-//   mana: 6,
-//   level: 3,
-//   tipo: "Arqueiro",
-// };
+const personagens = [personagemPedrinho, personagemJose];
 
-const personagemPedrinho = new Personagem();
-personagemPedrinho.nome = "Pedrinho";
-personagemPedrinho.vida = 7;
-personagemPedrinho.mana = 12;
-personagemPedrinho.tipo = "Mago";
-personagemPedrinho.level = 5;
-
-const personagemJose = new Personagem();
-personagemJose.nome = "José";
-personagemJose.vida = 7;
-personagemJose.mana = 6;
-personagemJose.tipo = "Arqueiro";
-personagemJose.level = 3;
-
-console.log(
-  "Insignia de " +
-    personagemPedrinho.nome +
-    ": " +
-    personagemPedrinho.obterInsignia()
-);
-
-console.log(
-  "Insignia de " + personagemJose.nome + ": " + personagemJose.obterInsignia()
-);
+new PersonagemView(personagens).render();
