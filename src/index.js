61e75eaf9b96e69ed69fc8c633955f955fe8cc42
Pedro Xavier <pedroxxavier@hotmail.com>
2023-08-23 15:05:21 -0300

    Fundamentos do POO

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { Personagem } from "./modules/personagem.js";
 import { PersonagemView } from "./components/personagem-view.js";
 import { Mago } from "./modules/mago.js";
+import { Arqueiro } from "./modules/arqueiro.js";
+import { ArqueiroMago } from "./modules/arqueiro-mago.js";
 
-const personagemPedrinho = new Personagem("Pedrinho", 5, "Mago");
-const personagemJose = new Personagem("José", 3, "Arqueiro");
+const magoAlan = new Mago("Alanzoka", 4, "Fogo", 4, 1);
+const magaRethe = new Mago("Maethe", 6, "Gelo", 5, 7);
+const arquEllie = new Arqueiro("Ellie", 7, 8);
+const arqueArmago = new ArqueiroMago("Boquinha de Escoto", 9, 4, "Ar", 10, 9);
 
-const personagens = [personagemPedrinho, personagemJose];
+const personagens = [magoAlan, magaRethe, arquEllie, arqueArmago];
 
 new PersonagemView(personagens).render();
